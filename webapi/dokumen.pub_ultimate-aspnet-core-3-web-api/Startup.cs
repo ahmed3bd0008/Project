fbe96a592f801b8da68f38ccc553c3a622d3ef46
Ahmed abdel Ghafar Ali <AhmedG@hits-egypt.local>
2021-09-09 16:48:19 +0200

    adding class library files

--- webapi/dokumen.pub_ultimate-aspnet-core-3-web-api/Startup.cs
+++ webapi/dokumen.pub_ultimate-aspnet-core-3-web-api/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -45,7 +46,10 @@ namespace dokumen.pub_ultimate_aspnet_core_3_web_api
             }
 
             app.UseHttpsRedirection();
-
+            app.UseStaticFiles();
+            app.UseForwardedHeaders(new ForwardedHeadersOptions(){
+                ForwardedHeaders=ForwardedHeaders.All
+            });
             app.UseRouting();
 
             app.UseAuthorization();
