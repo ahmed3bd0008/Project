7535d90b1673c65fec60a19407cc43cf16717d06
Ahmed abdel Ghafar Ali <AhmedG@hits-egypt.local>
2021-09-12 16:38:32 +0200

    sql connection

--- webapi/dokumen.pub_ultimate-aspnet-core-3-web-api/Extension/serviceExtenision.cs
+++ webapi/dokumen.pub_ultimate-aspnet-core-3-web-api/Extension/serviceExtenision.cs
@@ -1,3 +1,5 @@
+using Entity.Context;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -5,9 +7,9 @@ namespace dokumen.pub_ultimate.Extension
 {
     public static class serviceExtension
     {
-                public static void ConfigurationSqlServer(this IServiceCollection services,IConfiguration configuration)
-                {
-
-                }
+            public static void ConfigurationSqlServer(this IServiceCollection services,IConfiguration configuration)
+             {
+                services.AddDbContext<RepoDbContext>(opts => opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"),b=>b.MigrationsAssembly("Entities")));
+             }
     }
 }
