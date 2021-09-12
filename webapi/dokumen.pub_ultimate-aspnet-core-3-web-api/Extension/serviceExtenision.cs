21e0d33221331cc2b6af9b2bd69af1d62d33c6fa
Ahmed abdel Ghafar Ali <AhmedG@hits-egypt.local>
2021-09-12 15:36:43 +0200

    sql connection

---
+++ webapi/dokumen.pub_ultimate-aspnet-core-3-web-api/Extension/serviceExtenision.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace dokumen.pub_ultimate.Extension
+{
+    public static class serviceExtension
+    {
+                public static void ConfigurationSqlServer(this IServiceCollection services,IConfiguration configuration)
+                {
+
+                }
+    }
+}
