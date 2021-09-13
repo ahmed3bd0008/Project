7db5e4051ad52c8e3225edfbcd2efea306cf28c0
Ahmed abdel Ghafar Ali <AhmedG@hits-egypt.local>
2021-09-13 14:31:52 +0200

    sql connection

--- webapi/dokumen.pub_ultimate-aspnet-core-3-web-api/Startup.cs
+++ webapi/dokumen.pub_ultimate-aspnet-core-3-web-api/Startup.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dokumen.pub_ultimate.Extension;
+using Entity.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,6 +32,7 @@ namespace dokumen.pub_ultimate_aspnet_core_3_web_api
         {
 
             services.AddControllers();
+            services.ConfigurationSqlServer(Configuration);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "dokumen.pub_ultimate_aspnet_core_3_web_api", Version = "v1" });
